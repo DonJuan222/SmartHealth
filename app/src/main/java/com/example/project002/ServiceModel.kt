adefb1c7970455bef6b109c33524e0ef315eb1b8
“donpolo” <poli123hj@gmail.com>
2022-09-19 19:51:11 -0500

    se fue de nuevo

--- app/src/main/java/com/example/project002/ServiceModel.kt
+++ app/src/main/java/com/example/project002/ServiceModel.kt
@@ -2,8 +2,8 @@ package com.example.project002
 
 data class ServiceModel (
     val id: String,
-    val icon: String,
     val title: String,
-    val description: String
+    val description: String,
+    val icon: String
 
 )
