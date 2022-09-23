f5bf07698a370c71cb563422105abe828eaa783a
“donpolo” <poli123hj@gmail.com>
2022-09-22 22:32:14 -0500

    Un commit más

--- app/src/main/java/com/example/project002/LocationFragment.kt
+++ app/src/main/java/com/example/project002/LocationFragment.kt
@@ -6,12 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.project002.databinding.FragmentLocationBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class BlankFragment : Fragment() {
+class BlankFragment : Fragment(), OnMapReadyCallback {
 
     private var  _binding: FragmentLocationBinding? =null
     private val binding: FragmentLocationBinding get() =  _binding!!
+    private lateinit var mMap:GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +26,24 @@ class BlankFragment : Fragment() {
     ): View? {
         _binding=FragmentLocationBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val mapFragment:SupportMapFragment = childFragmentManager.findFragmentById(R.id.fragment_location_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        mMap = map
+        mMap.uiSettings.isScrollGesturesEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.mapType=GoogleMap.MAP_TYPE_SATELLITE
+        val  lating:LatLng=LatLng(3.0178418,-76.4820921)
+        mMap.addMarker(MarkerOptions().position(lating)
+            .title("SaludApp"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lating, 12.0f))
     }
 }
--- app/src/main/java/com/example/project002/SpecialistFragment.kt
+++ app/src/main/java/com/example/project002/SpecialistFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.project002.databinding.FragmentLocationBinding
 import com.example.project002.databinding.FragmentSpecialistBinding
 
 
