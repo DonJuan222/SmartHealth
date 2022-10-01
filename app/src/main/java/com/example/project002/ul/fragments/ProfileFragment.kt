4a6fd888a474e07aa0256925a1b8251cffac5dc3
David_Quintero <perrex17@gmail.com>
2022-10-01 12:07:33 -0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/project002/ul/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/project002/ul/fragments/ProfileFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import com.example.project002.data.viewmodels.LoginViewModel
 import com.example.project002.databinding.FragmentProfileBinding
@@ -26,6 +27,7 @@ class ProfileFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+
     }
 
 
